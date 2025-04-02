cfa6f986ee1e9284ba6b393667590c3ea7392ede
TylerDavidBailey <TylerDavidBailey@gmail.com>
2025-04-02 16:55:46 -0400

    fix

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
+	"syscall/js"
+)
+
+var counter int
+
+func increment(this js.Value, p []js.Value) interface{} {
+	counter++
+	js.Global().Get("document").Call("getElementById", "counter").Set("innerText", strconv.Itoa(counter))
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, WebAssembly!")
+	// Set initial counter value in the HTML
+	js.Global().Get("document").Call("getElementById", "counter").Set("innerText", strconv.Itoa(counter))
+
+	// Bind the increment function to the button's click event
+	cb := js.FuncOf(increment)
+	defer cb.Release()
+	js.Global().Get("document").Call("getElementById", "incrementButton").Call("addEventListener", "click", cb)
+
+	// Prevent the Go program from exiting
+	select {}
 }
