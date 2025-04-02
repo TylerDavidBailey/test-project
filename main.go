844bd55b7b3e2d9c9b9c7d1b0b11fb0b4c82c52a
TylerDavidBailey <TylerDavidBailey@gmail.com>
2025-04-02 16:15:49 -0400

    fix

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"syscall/js"
-)
-
-var counter int
-
-func increment(this js.Value, p []js.Value) interface{} {
-	counter++
-	js.Global().Get("document").Call("getElementById", "counter").Set("innerText", counter)
-	return nil
-}
+import "fmt"
 
 func main() {
-	c := make(chan struct{}, 0)
-	js.Global().Set("increment", js.FuncOf(increment))
-	<-c
+	fmt.Println("Hello, WebAssembly!")
 }
