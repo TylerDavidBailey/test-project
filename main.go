25979a056579880f4a0bdcbaedd3a2d41b10e659
TylerDavidBailey <TylerDavidBailey@gmail.com>
2025-04-04 16:25:13 -0400

    n

--- main.go
+++ main.go
@@ -8,14 +8,24 @@ import (
 	"syscall/js"
 )
 
-func encodeBase64(this js.Value, args []js.Value) any {
+type Args struct {
+	Input string
+}
+
+func getArgsFromDOM() Args {
 	document := js.Global().Get("document")
-
 	input := document.Call("getElementById", "inputText").Get("value").String()
-	slog.Info("encoding this value", slog.Any("input", input))
-	encoded := base64.StdEncoding.EncodeToString([]byte(input))
-	slog.Info("encoded value", slog.Any("encoded", encoded))
+	return Args{Input: input}
+}
 
+func encodeBase64(this js.Value, args []js.Value) any {
+	argData := getArgsFromDOM()
+
+	slog.Info("encodeBase64", slog.Any("argData", argData))
+
+	encoded := base64.StdEncoding.EncodeToString([]byte(argData.Input))
+
+	document := js.Global().Get("document")
 	outputElem := document.Call("getElementById", "output")
 	outputElem.Set("innerHTML", encoded)
 
@@ -28,7 +38,6 @@ func main() {
 	c := make(chan struct{}, 0)
 
 	document := js.Global().Get("document")
-
 	encodeBtn := document.Call("getElementById", "encode")
 	encodeBtn.Call("addEventListener", "click", js.FuncOf(encodeBase64))
 
