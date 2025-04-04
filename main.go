6ddf03249b0b543bb671191dede3e90da1e0ec09
TylerDavidBailey <TylerDavidBailey@gmail.com>
2025-04-04 14:20:53 -0400

    logging

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"encoding/base64"
+	"log/slog"
 	"syscall/js"
 )
 
@@ -11,7 +12,9 @@ func encodeBase64(this js.Value, args []js.Value) any {
 	document := js.Global().Get("document")
 
 	input := document.Call("getElementById", "inputText").Get("value").String()
+	slog.Info("encoding this value", slog.Any("input", input))
 	encoded := base64.StdEncoding.EncodeToString([]byte(input))
+	slog.Info("encoded value", slog.Any("encoded", encoded))
 
 	outputElem := document.Call("getElementById", "output")
 	outputElem.Set("innerHTML", encoded)
@@ -20,6 +23,8 @@ func encodeBase64(this js.Value, args []js.Value) any {
 }
 
 func main() {
+	slog.Info("WASM app starting")
+
 	c := make(chan struct{}, 0)
 
 	document := js.Global().Get("document")
