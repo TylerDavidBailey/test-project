7dac07ccbbd3b9484d4b9a7179aaa32ff241b4e9
TylerDavidBailey <TylerDavidBailey@gmail.com>
2025-04-04 14:12:05 -0400

    encoder

--- main.go
+++ main.go
@@ -3,26 +3,19 @@
 package main
 
 import (
+	"encoding/base64"
 	"syscall/js"
 )
 
-var count int
-
-func updateCounterDisplay() {
+func encodeBase64(this js.Value, args []js.Value) any {
 	document := js.Global().Get("document")
-	counterText := document.Call("getElementById", "counter")
-	counterText.Set("innerHTML", count)
-}
 
-func increment(this js.Value, args []js.Value) any {
-	count++
-	updateCounterDisplay()
-	return nil
-}
+	input := document.Call("getElementById", "inputText").Get("value").String()
+	encoded := base64.StdEncoding.EncodeToString([]byte(input))
 
-func clear(this js.Value, args []js.Value) any {
-	count = 0
-	updateCounterDisplay()
+	outputElem := document.Call("getElementById", "output")
+	outputElem.Set("innerHTML", encoded)
+
 	return nil
 }
 
@@ -31,16 +24,8 @@ func main() {
 
 	document := js.Global().Get("document")
 
-	// Bind increment button
-	incrBtn := document.Call("getElementById", "increment")
-	incrBtn.Call("addEventListener", "click", js.FuncOf(increment))
+	encodeBtn := document.Call("getElementById", "encode")
+	encodeBtn.Call("addEventListener", "click", js.FuncOf(encodeBase64))
 
-	// Bind clear button
-	clearBtn := document.Call("getElementById", "clear")
-	clearBtn.Call("addEventListener", "click", js.FuncOf(clear))
-
-	// Initialize display
-	updateCounterDisplay()
-
-	<-c // Prevent main from exiting
+	<-c
 }
