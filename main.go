9954b7708aa1030a4586a549c8e874024f12c5fd
TylerDavidBailey <TylerDavidBailey@gmail.com>
2025-04-09 21:27:29 -0400

    fix

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-//go:build js && wasm
-
 package main
 
 import (
@@ -14,21 +12,14 @@ func generateWorkout(this js.Value, args []js.Value) any {
 		"Plank (sec):30",
 	}
 
-	// Convert Go slice to JS array
 	jsArray := js.Global().Get("Array").New(len(workout))
 	for i, item := range workout {
 		jsArray.SetIndex(i, item)
 	}
-
 	return jsArray
 }
 
-func registerCallbacks() {
-	js.Global().Set("generateWorkout", js.FuncOf(generateWorkout))
-}
-
 func main() {
-	c := make(chan struct{}, 0)
-	registerCallbacks()
-	<-c
+	js.Global().Set("generateWorkout", js.FuncOf(generateWorkout))
+	select {}
 }
